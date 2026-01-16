79c6cd7dc9f4aad78e0f38968dd6a7914a8f220f
SOURABH-MALHOTRA <malhotrasourabh034@gmail.com>
2026-01-16 23:57:30 +0530

    added base path for deployment

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  base: '/Webzon/', // Aapke repository ka naam yahan aayega
 })
