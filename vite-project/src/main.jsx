5766359b1b60aa7dc74e38b298a23e0f4c5e6d00
SOURABH MALHOTRA <162816769+SOURABH-MALHOTRA@users.noreply.github.com>
2026-01-17 01:14:15 +0530

    Update BrowserRouter with basename for routing

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -6,8 +6,11 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+<BrowserRouter basename="/Webzon">
+  <App />
+</BrowserRouter>
   </React.StrictMode>
 );
+import { BrowserRouter } from 'react-router-dom';
+
+// Aisa dikhna chahiye:
