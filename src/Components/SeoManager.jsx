43b9db7aedb5528bb8db78de079501809c469165
SOURABH-MALHOTRA <malhotrasourabh034@gmail.com>
2026-05-02 21:48:41 +0530

    seo

--- src/Components/SeoManager.jsx
+++ src/Components/SeoManager.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const BASE_URL = "https://webzon.in";
+const BASE_URL = "https://webzon.co.in";
 
 const pageSeo = {
   "/": {
